74a149f0cef46a422c4400d6f1071a7b21ad5abf
James <pokinoice@live.com>
2022-07-16 10:49:50 +0800

    Push

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+function getRootElement() {
+  const existing_el = document.getElementById('root');
+  if (existing_el) {
+    return existing_el;
+  }
+
+  const new_el = document.createElement('div');
+  new_el.id = 'root';
+  document.body.appendChild(new_el);
+
+  return new_el;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = createRoot(getRootElement());
+  root.render(<div>Hello</div>);
+});
